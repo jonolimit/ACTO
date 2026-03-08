2f2c0de151d88eedef5648f8a22c336b26c7bdb0
Aldi saputra <sagirialdi@gmail.com>
2026-03-08 14:08:51 -0700

    update dashboard url to railway

--- acto-web/src/config.ts
+++ acto-web/src/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   social: {
     x: ' https://x.com/actobotgg?s=21',
-    github: 'https://github.com/actobotics/ACTO',
+    github: 'https://github.com/0xzonee/ACTO',
   },
   links: {
-    docs: 'https://github.com/actobotics/ACTO/blob/main/README.md',
-    repository: 'https://github.com/actobotics/ACTO',
+    docs: 'https://github.com/0xzonee/ACTO/blob/main/README.md',
+    repository: 'https://github.com/0xzonee/ACTO',
     dashboard: 'https://acto-production.up.railway.app/dashboard',
     pypi: 'https://pypi.org/project/actobotics/',
     api: 'https://acto-production.up.railway.app',
